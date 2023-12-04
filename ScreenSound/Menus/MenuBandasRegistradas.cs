5ea5d3770755848eadeaa5250f0f7e52a05a0705
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-12-04 19:40:54 -0300

    Isolando o menu de opções

---
+++ ScreenSound/Menus/MenuBandasRegistradas.cs
@@ -0,0 +1,23 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuBandasRegistradas : Menu
+{
+    public void Executar(Dictionary<String,Banda> bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTituloDaOpcao("Exibindo todas as bandas registradas na nossa aplicação");
+
+        foreach (string banda in bandasRegistradas.Keys)
+        {
+            Console.WriteLine($"Banda: {banda}");
+        }
+
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        
+
+    }
+}
---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,9 @@
+﻿namespace ScreenSound.Menus;
+
+internal class MenuSair : Menu
+{
+    internal void Executar()
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
